f61b06c93860a4ed18e7a30ce8f2618ecb8e9ae3
gruzja <franciszek.maslon@cyfroweszkoly.pl>
2026-03-15 18:41:56 +0100

    isplay calculation result or error message in UI

--- RapidPack/MainWindow.axaml.cs
+++ RapidPack/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia.Controls;
 
 namespace RapidPack;
@@ -8,14 +9,15 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
     public void CalculateButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
-        
+
         string wagaTxt = WeightTextBox.Text;
         string wysTxt = HeightTextBox.Text;
         string szerTxt = WidthTextBox.Text;
         string glebTxt = DepthTextBox.Text;
-        
+
         if (!double.TryParse(WeightTextBox.Text, out double waga) ||
             !double.TryParse(HeightTextBox.Text, out double wys) ||
             !double.TryParse(WidthTextBox.Text, out double szer) ||
@@ -25,11 +27,25 @@ public partial class MainWindow : Window
             ResultTextBlock.Text = "Błąd: Wprowadź poprawne liczby!";
             return;
         }
-        var comboItem = (ComboBoxItem)ShipmentTypeComboBox.SelectedItem;
-        string typ = comboItem?.Content?.ToString() ?? "Standardowa";
-        bool czyEkspres = ExpressCheckBox.IsChecked ?? false;
 
-        var calculator = new ParcelCalculator();
-        double wynik = calculator.Oblicz(waga, szer, wys, gleb, czyEkspres, typ);
+        if (waga > 30)
+        {
+            ResultBorder.IsVisible = true;
+            ResultTextBlock.Text = "Błąd: Paczka jest za ciężka (max 30kg)!";
+        }
+        else
+        {
+
+            var comboItem = (ComboBoxItem)ShipmentTypeComboBox.SelectedItem;
+            string typ = comboItem?.Content?.ToString() ?? "Standardowa";
+            bool czyEkspres = ExpressCheckBox.IsChecked ?? false;
+
+            var calculator = new ParcelCalculator();
+            double wynik = calculator.Oblicz(waga, szer, wys, gleb, czyEkspres, typ);
+
+
+            ResultBorder.IsVisible = true;
+            ResultTextBlock.Text = "Do zapłaty: " + wynik + " zł";
+        }
     }
 }
